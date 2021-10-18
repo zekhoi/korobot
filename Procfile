998534e805125891efefa18fd9b8889b6d41b7bf
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-10-18 08:55:56 +0800

    update procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+heroku ps:scale --app zekhoi-korobot worker=1
 worker : node index.js
--- src/command.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-export async function getMeme(){
-    const res = await axios.get('https://memeapi.pythonanywhere.com/');
-    return res.data.memes[0].url;
-}
-
-module.exports = getMeme
