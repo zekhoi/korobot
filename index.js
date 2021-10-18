b24892aa3a20a07e315369f218a929ff91815961
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-10-19 02:55:25 +0800

    mass update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, MessageEmbed } = require("discord.js");
-const BOT_TOKEN = "ODkzNDMxMjEzNDMwMjg4NDE0.YVbWqA.2xDf1RivgmcWzqSAmh62NWDw33Y";
+const config = require("./config.json");
+const data = require("./data.json");
 
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
@@ -18,30 +19,88 @@ client.once('ready', () => {
 });
 
 client.on('message', async message => {
-    content = message.content.toLowerCase()
-
-    if(message.mentions.everyone){
-        message.reply(`Ada perlu apa ${message.author} ngemention semuanya?`);
-    }
-
-    if (!content.startsWith(prefix) || message.author.bot) return;
-    
+    let content = message.content.toLowerCase()
+    let filter = content.split(' ');
     const commandBody = content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    // console.log(args)
+    // console.log(content)
+    // console.log(message)
     try {
 
-        if (message.mentions.has(client.user)) {
-            message.reply('Kenapa? Ada yang mau gelud kah?');
+        if(filter.includes("loli") || filter.includes("fbi")){
+            let department = data.department
+            let agent = department[Math.floor(Math.random()*department.length)]
+            let fbi = new MessageEmbed()
+                .setColor("#ff00dd")
+                .setDescription(`:man_police_officer: **${agent.halo}**`)
+                .setImage(agent.image)
+                message.channel.send({ embeds: [fbi] });
         }
+
+        // if(message.author.id == 878634991620345887){
+        //     let kalimat = [
+        //         "Iya maaf kak",
+        //         "Aku dibuang mulu sih kak",
+        //         "Jangan tsundere mulu dong kak",
+        //         "Lagi ngapain kak?",
+        //         "Iri ya kak?",
+        //         "Udah makan belom kak?",
+        //         "Lagi galau ya kak?",
+        //         "Marah-marah mulu",
+        //         "Mau seblak?",
+        //     ]
+        //     let ngeselin = kalimat[Math.floor(Math.random()*kalimat.length)]
+        //     message.reply(ngeselin);
+        //     return;
+        // }
+
+        if(message.mentions.everyone){
+            message.reply(`Ada perlu apa ${message.author} manggil semua orang? Mau tawuran kah?`);
+            return;
+        }
+
+        if (message.mentions.has(client.user)) {
+             //fetches an URL from the API
+            let kata = data.kata_kata
+            let rep = kata[Math.floor(Math.random()*kata.length)]
+            message.reply(rep);
+            return;
+        }
+
+        if (!content.startsWith(prefix) || message.author.bot) return;
 
         switch (command) {
 
+            case "help":
+                let helper = new MessageEmbed()
+                .setColor("#ff00dd")
+                .setAuthor("Command List", "https://cdn.discordapp.com/attachments/896402692925190167/899665193037099058/attachment_126736972.jpeg")
+                .setDescription("Gak ada fitur apa-apa, cuma gabut aja mageran.")
+                .addFields(
+                    { name: '**Mandi**', value: '`koro mandi <mention>`' },
+                    { name: '**Nyengir**', value: '`koro nyengir`' },
+                    { name: '**Yeet**', value: '`koro yeet`' },
+                    { name: '**Lightstick**', value: '`koro ls`' },
+                )
+                .setFooter('Made by koro', 'https://cdn.discordapp.com/attachments/896402692925190167/899665193037099058/attachment_126736972.jpeg');
+                message.channel.send({ embeds: [helper] });
+                break;
+
+
+            case "yeet":
+                let yeet = "https://c.tenor.com/jSx1KiL3L2UAAAAC/yeet-lion-king.gif"
+                message.channel.send(yeet);
+                break;
+    
             case "jess":
                 message.channel.send("Jess cuma punya Koro!");
                 break;
-    
+
+            case "rana":
+                message.reply("Hmmmm");
+                break;
+
             case "ls":
                 let ls = [
                     "https://c.tenor.com/zWF-SI9SQdwAAAAC/cat-dancing.gif"
@@ -52,7 +111,7 @@ client.on('message', async message => {
                 break;
             
             case "ngeselin":
-                message.reply("Aku dibuang terus"); //send the image URL
+                message.reply("Iya kak maaf :("); //send the image URL
                 break;
 
             case "gelud":
@@ -61,11 +120,7 @@ client.on('message', async message => {
                 break;
 
             case "nyengir":
-                let images = ["https://media.discordapp.net/attachments/896402692925190167/899309006663847997/IMG_20211008_185048.jpg",
-                    "https://i.pinimg.com/originals/90/1d/fc/901dfca7406689e2ba859bc7bda8c7b8.png",
-                    "https://www.memesmonkey.com/images/memesmonkey/s_38/3880a4f9952272933965427934eb5b17.jpeg",
-                    "https://c.tenor.com/rsoqDTrdsIUAAAAM/cat-smile.gif"
-                ];
+                let images = data.nyengir
                 let nyengir = images[Math.floor(Math.random()*images.length)]
                 message.channel.send(nyengir); //send the image URL
                 break;
@@ -77,15 +132,10 @@ client.on('message', async message => {
                     break;
                 }
 
-                let bath = [
-                    "https://c.tenor.com/KhNWlWQ9ST4AAAAM/shaptic-monk.gif",
-                    "https://c.tenor.com/jMrp5EU-i2QAAAAd/water-monkey.gif",
-                    "https://c.tenor.com/XlINniyLnowAAAAC/monkey-in.gif",
-                    "https://c.tenor.com/I26DqtPNzyUAAAAC/animals-cute.gif",
-                    "https://c.tenor.com/9rkrXs9nbNQAAAAd/monkey-monkeys.gif",
-                ];
+                let bath = data.mandi
                 let mandi = bath[Math.floor(Math.random()*bath.length)]
                 let embed = new MessageEmbed()
+                .setColor("#ff00dd")
                 .setDescription(`:bathtub:  **${message.author.username}** mandiin **${mentioned.username}**`)
                 .setImage(mandi)
                 .setTimestamp()
@@ -101,4 +151,4 @@ client.on('message', async message => {
     
 })
 
-client.login(BOT_TOKEN);
+client.login(config.BOT_TOKEN);
